e16d7736d46224cf71ec9b722bc14bca771b2339
lukedoolittle <luke.doolittle@gmail.com>
2017-07-26 11:52:46 -0500

    recorrect

--- exercise_2/postgres_setup.py
+++ exercise_2/postgres_setup.py
@@ -1,37 +1,32 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-try:
-    conn = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="pass",
-        host="localhost",
-        port="5432")
+conn = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="pass",
+    host="localhost",
+    port="5432")
+try: 
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
-    cur.execute('CREATE DATABASE tcount')
+    cur.execute("CREATE DATABASE tcount")
+    cur.close()
+    conn.close()
     print("Created database tcount")
-except:
+except: 
     print("Could not create tcount")
-finally:
-    conn.close()
 
-try:
-    conn = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="pass",
-        host="localhost",
-        port="5432")
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE tweetwordcount
-        (word TEXT PRIMARY KEY     NOT NULL,
-        count INT     NOT NULL)''')
-    conn.commit()
-    print('Created table tweetwordcount')
-except:
-    print("Could not create tcount")
-finally:
-    conn.close()
-    
+conn = psycopg2.connect(
+    database="tcount",
+    user="postgres",
+    password="pass",
+    host="localhost",
+    port="5432")
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE tweetwordcount 
+       (word TEXT PRIMARY KEY     NOT NULL, 
+       count INT     NOT NULL);''') 
+conn.commit() 
+print("Created table tweetwordcount")
